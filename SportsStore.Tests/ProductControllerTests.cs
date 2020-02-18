f0efd0c9d214aa214aad895f5be151d7b236a5b1
kemal.genc <kg800657@ZK34ZT044NB>
2020-02-18 13:19:27 +0300

    first commit for local

---
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void Can_Paginate() {
+            //Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            }).AsQueryable<Product>());
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            //Act
+            var result = controller.List(2).ViewData.Model as ProductsListViewModel;
+
+            //Assert
+            Product[] products = result.Products.ToArray();
+            Assert.True(products.Length == 2);
+            Assert.Equal("P4", products[0].Name);
+            Assert.Equal("P5", products[1].Name);
+        }
+    }
+}
